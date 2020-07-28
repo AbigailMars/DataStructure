57547c7a7a98bb0a76191d985e7b12fd4fefdcff
DinosaurMars <62925825+AbigailMars@users.noreply.github.com>
2020-07-28 21:05:00 +0800

    Update simple1 .cpp

--- 美国两人选举平票问题/simple1 .cpp
+++ 美国两人选举平票问题/simple1 .cpp
@@ -1,8 +1,2 @@
-int countOnes(unsigned int n ){
-	int ones = 0;
-	while(0 < n ){
-	count += (n&1);
-	n >>= 1;
-	}
-	return ones;
-}
+//两位候选人参选，并争夺n = 51 个选举人团（ 50 个州和 1 个特区）的共计 2m = 538 张选举人票，是否可能因两人恰好各得m = 269 张，而不得不重新选举？
+ //NP-Complete ,无法在多项式时间内回答此问题，基于直觉的蛮力算法已属最优 O(2^n)
