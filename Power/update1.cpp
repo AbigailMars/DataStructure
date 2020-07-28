35f55a02ae1a08c656a822be3c018733aac63161
DinosaurMars <62925825+AbigailMars@users.noreply.github.com>
2020-07-28 15:55:41 +0800

    Update update1.cpp

--- Power/update1.cpp
+++ Power/update1.cpp
@@ -1,7 +1,5 @@
-int countOne1(unsigned int n ){
-	int ones = 0;
-	while(0 < n){
-	ones++;
-    n &= 
-    return ones;
-}
+inline int sqr(int a) {return a*a;}
+int power2(int n){
+    if(0 == n) return 1;
+    return (n & 1) ? sqr(power2(n >>1)) <<1 : sqr(power2(n>>1));
+}//O(log(n)) = O(r) r为输入指数n的比特位数
